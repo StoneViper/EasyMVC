e8b424ffe4089d94a71b1a4998678d5348a65bed
Catontree <oilgame@yeah.net>
2018-08-29 14:49:18 +0800

    init

---
+++ framework/core/Framework.class.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace framework\core;
+
+class Framework
+{
+	public static function run()
+	{
+		echo "HELLO WORLD!";
+	}
+}
+
+
+
+ ?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+/**
+ * 入口文件
+ */
+
+require_once 'framework/core/Framework.class.php';
+
+framework\core\Framework::run();
